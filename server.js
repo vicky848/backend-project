755ab3b96404f01d149cbc2b314462dcc9d95403
vicky <vickychowrsaia35@gmail.com>
2025-04-18 22:15:06 +0530

    Add CORS support to backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
 
 const {open} = require("sqlite");
 const sqlite3 = require("sqlite3");
@@ -8,7 +9,7 @@ const app = express();
 const PORT = 3000; 
 
 app.use(express.json());
-
+app.use(cors())
 const dbPath = path.join(__dirname, "product.db");
 
 let db = null ; 
